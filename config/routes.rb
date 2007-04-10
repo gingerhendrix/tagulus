ba1c88f2d6adfed713c70db7ec82d6ae7af748ca
Gareth Andrew <gingerhendrix@hotmail.com>
2007-04-10 12:24:43 +0000

    v0.1 simple styling
    git-svn-id: svn+ssh://s1439.gridserver.com/home/1439/users/.home/repos/Tagulus/head@35 5d58421e-2428-0410-a994-821f96727b03

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,5 @@ ActionController::Routing::Routes.draw do |map|
   map.connect ':controller/:action/:id.:format'
   map.connect ':controller/:action/:id'
   
-  map.connect '', :controller => "cloud", :action => "list"
+  map.connect '', :controller => "cloud", :action => "index"
 end
